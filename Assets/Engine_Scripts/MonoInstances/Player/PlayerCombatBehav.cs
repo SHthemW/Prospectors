8e4ca0d5aea1521d01e9e99ba7854f61a44cc578
SHthemW <2536076378@qq.com>
2023-11-09 19:12:34 +0800

    opt: 分离了player的战斗逻辑

--- Assets/Engine_Scripts/MonoInstances/Player/PlayerCombatBehav.cs
+++ Assets/Engine_Scripts/MonoInstances/Player/PlayerCombatBehav.cs
@@ -1,4 +1,5 @@
 ﻿using Game.Interfaces.GameObj;
+using Game.Services.Combat;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,19 @@ namespace Game.Instances.Player
 {
     internal sealed class PlayerCombatBehav : PlayerBehaviour, IWeaponAimMaster
     {
+        private WeaponAimTurnUpdater _aimTurnUpdater;
+
         Vector3 IWeaponAimMaster.AimingPosition => Components.AimPoint.transform.position;
+
+        private void Awake()
+        {
+            Components.AimPoint.Init(Components.RootTransform, DataHandler.MaxFollowOffsetDuringAim);
+
+            _aimTurnUpdater = new(Components.AimPoint, Components.AimBone, DataHandler.AimHeight);
+        }
+        private void Update()
+        {           
+            _aimTurnUpdater.UpdateAimTurn();
+        }
     }
 }
