865fad9c948fc7756168291a5f79e1b7f1c2c863
SHthemW <2536076378@qq.com>
2023-11-09 18:36:19 +0800

    dev: 可跟随旋转的独立炮筒

---
+++ Assets/Engine_Scripts/MonoInstances/Combat/RotatableWeapon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Instances.Combat
+{
+    internal sealed class RotatableWeapon : MonoBehaviour
+    {
+        [field: SerializeField]
+        private Transform _followPoint;
+
+        private void Update()
+        {
+            transform.forward = _followPoint.position - transform.position;
+        }
+    }
+}
