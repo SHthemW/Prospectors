e8e36a41fa29706e63bfb141c18051d255689335
SHthemW <2536076378@qq.com>
2023-11-13 15:22:57 +0800

    opt: 优化WeaponData_SO的数据格式

--- Assets/Engine_Scripts/Services/Combat/datas/WeaponData_SO.cs
+++ Assets/Engine_Scripts/Services/Combat/datas/WeaponData_SO.cs
@@ -7,7 +7,24 @@ namespace Game.Services.Combat
     [CreateAssetMenu(fileName = "new Weapon", menuName = "Data/Combat/Weapon")]
     public sealed class WeaponData_SO : ScriptableObject
     {
-        [field: SerializeField]
-        public GameObject Prefeb { get; private set; }
+        [Header("Assets")]
+
+        [SerializeField]
+        private GameObject _prefeb;
+
+        [Header("Runtime")]
+
+        [SerializeField]
+        private int _damage;
+
+        public GameObject Prefeb 
+        { 
+            get 
+            {
+                if (_prefeb == null)
+                    throw new NotImplementedException();
+                return _prefeb; 
+            } 
+        }
     }
 }
