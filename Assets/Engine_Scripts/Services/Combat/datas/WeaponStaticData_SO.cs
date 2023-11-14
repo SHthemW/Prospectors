90213565b85d60a31580f1a4b4b399f25d6d5001
SHthemW <2536076378@qq.com>
2023-11-14 16:52:53 +0800

    opt: 增加了对武器SO文件的边界值检查器

--- Assets/Engine_Scripts/Services/Combat/datas/WeaponStaticData_SO.cs
+++ Assets/Engine_Scripts/Services/Combat/datas/WeaponStaticData_SO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Game.Interfaces;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,20 +12,37 @@ namespace Game.Services.Combat
 
         [SerializeField]
         private GameObject _prefeb;
+        public GameObject Prefeb
+        {
+            get
+            {
+                if (_prefeb == null)
+                    throw new NotImplementedException();
+                return _prefeb;
+            }
+        }
+        private void CheckPrefeb()
+        {
+            if (_prefeb == null)
+            {        
+                Debug.LogWarning($"[data] {name} 的prefeb未配置.");
+                return;
+            }
+            if (!_prefeb.TryGetComponent<IWeapon>(out _))
+            {
+                Debug.LogWarning($"[data] {name} 的prefeb不是武器预制体.");
+                return;
+            }
+        }
 
         [Header("Runtime")]
 
         [SerializeField]
         private int _damage;
 
-        public GameObject Prefeb 
-        { 
-            get 
-            {
-                if (_prefeb == null)
-                    throw new NotImplementedException();
-                return _prefeb; 
-            } 
+        private void OnValidate()
+        {
+            CheckPrefeb();
         }
     }
 }
