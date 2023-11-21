42a424c56f300290cc81ef6041190d2e96c5441c
SHthemW <2536076378@qq.com>
2023-11-21 15:31:22 +0800

    opt: 改进PlayerComponents的Insp显示方式

--- Assets/Engine_Scripts/MonoInstances/Player/Basement/components/PlayerComponents.cs
+++ Assets/Engine_Scripts/MonoInstances/Player/Basement/components/PlayerComponents.cs
@@ -14,11 +14,6 @@ namespace Game.Instances.Player
             => _playerRb.TryGetIf(name, rb => rb != null);
 
         [SerializeField]
-        private Animator[] _charAnimators;
-        internal Animator[] CharAnimators 
-            => _charAnimators.TryGetIf(name, p => p != null && p.Length > 0);
-
-        [SerializeField]
         private Transform _rootTransform;
         internal Transform RootTransform 
             => _rootTransform.TryGetIf(name, p => p != null);
@@ -27,6 +22,13 @@ namespace Game.Instances.Player
         private Transform _weaponParent;
         internal Transform WeaponParent 
             => _weaponParent.TryGetIf(name, p => p != null);
+
+        [Header("Each Direction")]
+
+        [SerializeField]
+        private Animator[] _charAnimators;
+        internal Animator[] CharAnimators
+            => _charAnimators.TryGetIf(name, p => p != null && p.Length > 0);
 
         [SerializeField]
         private TaggedItem<GameObject>[] _charModels;
