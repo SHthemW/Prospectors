f597a30e8311ff877fafc2aca0c40454c2765368
SHthemW <2536076378@qq.com>
2023-11-21 17:20:07 +0800

    opt: 重命名一次错误的命名空间

--- Assets/Engine_Scripts/Interfaces/Utils/ConsoleTextFormat.cs
+++ Assets/Engine_Scripts/Interfaces/Utils/ConsoleTextFormat.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Game.Util
+namespace Game.Utils
 {
     public readonly struct Fmt
     {
--- Assets/Engine_Scripts/Interfaces/Utils/InspectorPropertyUtil.cs
+++ Assets/Engine_Scripts/Interfaces/Utils/InspectorPropertyUtil.cs
@@ -1,4 +1,4 @@
-﻿using Game.Util;
+﻿using Game.Utils;
 using System;
 using UnityEngine;
 
