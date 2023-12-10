733ba2f15a2f9bdef7bf7ad5840e3145d220b591
S.H.W <2536076378@qq.com>
2023-12-10 13:39:29 +0800

    fix: 增加UnhitableTag, 修复子弹由于碰撞自身而消失的问题

--- Assets/Engine_Scripts/MonoInstances/Entities/Bullet/BulletCollisionBehav.cs
+++ Assets/Engine_Scripts/MonoInstances/Entities/Bullet/BulletCollisionBehav.cs
@@ -7,12 +7,21 @@ namespace Game.Instances.Combat
     [RequireComponent(typeof(IBullet))]
     internal sealed class BulletCollisionBehav : BulletBehaviour
     {
+        [SerializeField]
+        private bool _enableHitDebug;
+
         private const int OBSTACLE_TIMES_COST = 99;
 
         private void OnCollisionEnter(Collision collision)
         {
+            if (collision.gameObject.CompareTag("Unhitable"))
+                return;
+
             if (!collision.gameObject.CompareTag("Hitable"))
             {
+                if (_enableHitDebug)
+                    Debug.Log("[bullet] hitted: " + collision.gameObject.name);
+
                 ThisBullet.CurrentHitTimes += OBSTACLE_TIMES_COST;
                 return;
             }
