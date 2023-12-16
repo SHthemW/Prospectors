0eae38faf682008d40e6b9235a31d3fe90f3a4e8
S.H.W <2536076378@qq.com>
2023-12-16 13:51:26 +0800

    dev: 更好的LogState

--- Assets/Engine_Scripts/MonoInstances/General/FSM/warrior/TestLogState.cs
+++ Assets/Engine_Scripts/MonoInstances/General/FSM/warrior/TestLogState.cs
@@ -7,20 +7,22 @@ namespace Game.Instances.General.FSM
 {
     internal sealed class TestLogState : AnimationFSMState
     {
+        [SerializeField]
+        private bool _enableStateEnterLog;
+
+        [SerializeField]
+        private bool _enableStateUpdateLog;
+
         protected override sealed void EnterStateAction()
         {
-            if (_animator == null)
-                return;
-
-            Debug.Log(_animator.name + "is enter.");
+            if (_enableStateEnterLog)
+                Debug.Log(_animator.name + "is enter.");
         }
 
         protected override sealed void UpdateStateAction()
         {
-            if (_animator == null)
-                return;
-
-            Debug.Log(_animator.name + "is updating..");
+            if (_enableStateUpdateLog)
+                Debug.Log(_animator.name + "is updating..");
         }
     }
 }
