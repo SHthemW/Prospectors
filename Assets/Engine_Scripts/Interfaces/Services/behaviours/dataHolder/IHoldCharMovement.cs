e48a10e59d247a8e2ddd5d362b8a0c93d7144fdb
SHthemW <2536076378@qq.com>
2023-12-20 16:05:25 +0800

    dev: 角色移动组件接口

---
+++ Assets/Engine_Scripts/Interfaces/Services/behaviours/dataHolder/IHoldCharMovement.cs
@@ -0,0 +1,11 @@
+﻿using Game.Utils.Collections;
+using UnityEngine;
+
+namespace Game.Interfaces
+{
+    public interface IHoldCharMovement : IDataHolder
+    {
+        DynamicData<float> MoveSpeed { get; set; }
+        Vector3 MoveDirection { get; set; }
+    }
+}
