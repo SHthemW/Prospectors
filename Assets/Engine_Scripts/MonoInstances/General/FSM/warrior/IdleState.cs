f2082d5e686384256710e11582769e4c9adc0e57
S.H.W <2536076378@qq.com>
2023-12-30 17:36:52 +0800

    dev: 为idleState添加单独的factor, 修正滑步问题

--- Assets/Engine_Scripts/MonoInstances/General/FSM/warrior/IdleState.cs
+++ Assets/Engine_Scripts/MonoInstances/General/FSM/warrior/IdleState.cs
@@ -8,15 +8,25 @@ namespace Game.Instances.General.FSM
     internal sealed class IdleState : AnimationFSMState
     {
         private const float IDLE_TIME = 3f;
+        private const string FACTOR_NAME = "idle";
 
+        private IHoldCharMovement _charMovement;
         private float _remainIdleTime;
-
-        protected override void EnterStateAction()
+        
+        protected override sealed void Init(Animator obj)
         {
+            base.Init(obj);
+
+            _charMovement = GetHolderOnParent<IHoldCharMovement>(obj);
+        }
+
+        protected override sealed void EnterStateAction()
+        {
+            _charMovement.MoveSpeed.AddFactor(() => 0, FACTOR_NAME);
             _remainIdleTime = IDLE_TIME;
         }
 
-        protected override void UpdateStateAction()
+        protected override sealed void UpdateStateAction()
         {
             _remainIdleTime -= Time.deltaTime;
 
@@ -24,5 +34,9 @@ namespace Game.Instances.General.FSM
                 _animator.SetBool(_stateName.IdleNotPatrol, false);
         }
 
+        protected override sealed void ExitStateAction()
+        {
+            _charMovement.MoveSpeed.RemoveFactor(FACTOR_NAME);
+        }
     }
 }
