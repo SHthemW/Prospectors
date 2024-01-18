6d02b2b4b7b495eb4a9f90ac12be927a094d5d2f
S.H.W <2536076378@qq.com>
2024-01-18 18:02:59 +0800

    dev: batch deep clone

--- Assets/Engine_Scripts/Interfaces/Services/general/IDeepCloneable.cs
+++ Assets/Engine_Scripts/Interfaces/Services/general/IDeepCloneable.cs
@@ -1,7 +1,19 @@
-﻿namespace Game.Interfaces
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Game.Interfaces
 {
-    public interface IDeepCloneable<out T> where T : IDeepCloneable<T>
+    public interface IDeepCloneable<T> where T : IDeepCloneable<T>
     {
         T DeepClone();
+
+        static T[] BatchDeepClone(IEnumerable<T> input)
+        {
+            return input.Select(act => act.DeepClone()).ToArray();
+        }
+        static TChild[] BatchDeepClone<TChild>(IEnumerable<TChild> input) where TChild : T
+        {
+            return input.Select(act => (TChild)act.DeepClone()).ToArray();
+        }
     }
 }
